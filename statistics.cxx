56f33c4a667ab2e04ab87502e228eadd3ffd6434
Astroschlumpf <oliver.hielscher@uni-duesseldorf.de>
2015-11-13 10:40:45 +0100

    Homework 3, mit Kommentar im Code

--- statistics.cxx
+++ statistics.cxx
@@ -3,16 +3,49 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void fuellhorn(double* poi, int len){
+  for(int i=0; i<len; i++){
+    poi[i] = rand()/((double)RAND_MAX); // Zufallszahl auf [0,1] normiert
+  }
+}
+
+double mittwe(double* poi, int len){
+  double mwSu = 0;
+  for(int i=0; i<len; i++){
+    mwSu += poi[i]/((double)len); // Summe aus x[i]/N == 1/N * Summe fuer Mittelwert
+  }
+  return mwSu;
+}
+
+double varw(double* poi, int len, double miwe){
+  double vasu = 0;
+  for(int i=0; i<len; i++){
+    // Varianz v = Summe ((x[i]-m)^2/N)
+    vasu += (poi[i]-miwe)*(poi[i]-miwe)/((double)len);
+  }
+  return vasu;
+}
 
 int main(){
-   const int N = 100;
+   int N = 10;
    double p[N];
    double mean, var;
-
-   // Some lines here....
+   
+   fuellhorn(p,N); // Array mit Zufallszahlen fuellen
+   mean = mittwe(p,N); // Mittelwert
+   var = varw(p,N,mean); // Varianz aus Werten und Mittelwert
+   ////////////////// Note: /////////////////////
+    // Random is not random at all...
+    // p[0] = 0.840188, p[1] = 0.394383 ...
+    // for the first and every other time!!!
+    // => mean and var will always be the exact
+    // same values every time as well
+   //////////////////////////////////////////////
+   
+   // for(int i=0; i<N; i++){
+   //   cout << p[i] << endl; // debug
+   // }
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
